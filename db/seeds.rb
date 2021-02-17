9ec04af73a073bf051f361f9d003e905b7a7f822
Wong Zhi Kai <wzhikai@gmail.com>
2021-02-17 20:44:06 +0800

    Dune quotes, no UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+
+10.times do
+  Article.create(
+    title: Faker::Books::Dune.character,
+    content: Faker::Books::Dune.quote
+  )
+end
